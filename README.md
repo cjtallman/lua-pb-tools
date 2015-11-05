4490c17c318a6fe09262743eecf16e1ade07fa12
Chris Tallman <cjtallman@gmail.com>
2015-11-05 14:28:24 -0800

    Minor changes to the build script, ignore file, and readme.

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # lua-pb-tools
+
+This is a small set of tools and scripts for building lua-protobuf from [protoc-gen-lua](https://github.com/haiweizhang/protoc-gen-lua).
+
+# usage
+Run build.bat to auto checkout the repo from github and build everything.
--- build.bat
+++ build.bat
@@ -1,12 +1,24 @@
 SETLOCAL
 @ECHO OFF
+
+:: Clean up any previous builds
+RMDIR /Q /S compiled
+
+:: Get a clean checkout of lua-protobuf from github
 RMDIR /Q /S protoc-gen-lua
 git clone https://github.com/haiweizhang/protoc-gen-lua.git
 
+:: Load VS environment
 CALL "%VS100COMNTOOLS%\vsvars32.bat"
-rmdir /Q /S compiled
+
+:: Make build projects
 CALL premake5.exe vs2010
+
+:: Build the projects
 msbuild "build\protobuf.sln" /m
-rmdir /Q /S build
+
+:: Cleanup
+RMDIR /Q /S build
+RMDIR /Q /S protoc-gen-lua
 
 ENDLOCAL
